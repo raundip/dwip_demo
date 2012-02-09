9ab7c650e02db421695f571cfabadf83447ee3af
Dwip Biswas <dwipbiswas5@gmail.com>
2012-02-09 20:42:40 +0000

    demo testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DwipDemo::Application.config.secret_token = 'a092ffab762b0cccd23730dd705ad993d2de153eb42c354b9d70f7a4a74851e2ed21b6fa6af9d18cc7bb8e6de92eadd2dbae802652883fcf8bbdfc5945598880'
